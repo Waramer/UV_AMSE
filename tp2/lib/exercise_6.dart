41f97676a6414b553ea5332eb2b57b113880a087
Quentin Delezenne <quentin.delezenne@orange.fr>
2022-02-09 16:07:25 +0100

    TP2 Ex6a

--- tp2/lib/exercise_6.dart
+++ tp2/lib/exercise_6.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Exercise6a extends StatefulWidget {
   const Exercise6a({Key? key,}) : super(key: key);
@@ -7,14 +8,57 @@ class Exercise6a extends StatefulWidget {
 }
 
 class _Exercise6aState extends State<Exercise6a> {
+  List<Widget> tiles =
+      List<Widget>.generate(2, (index) => TileWidget(tile: Tile.randomColor(),));
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Exercice 6a"),
+    return Scaffold(
+      body: Row(children: tiles),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.sentiment_very_satisfied,
+        ),
+        onPressed: swapTiles,
+        backgroundColor: Colors.red.shade900,
+      ),
     );
+  }
+
+  swapTiles() {
+    setState(() {
+      tiles.insert(1, tiles.removeAt(0));
+    });
   }
 }
 
+class Tile {
+  Color? color;
+  Tile(this.color);
+  Tile.randomColor() {
+    color = Color.fromARGB(
+        255, Random().nextInt(255), Random().nextInt(255), Random().nextInt(255));
+  }
+}
+
+class TileWidget extends StatelessWidget {
+  const TileWidget({Key? key, required this.tile}) : super(key: key);
+  final Tile tile;
+  
+  @override
+  Widget build(BuildContext context) {
+    return coloredBox();
+  }
+
+  Widget coloredBox() {
+    return Container(
+      color: tile.color,
+      child: const Padding(
+        padding: EdgeInsets.all(70.0),
+      )
+    );
+  }
+}
 //---------------------------------------------------
 
 class Exercise6b extends StatefulWidget {
